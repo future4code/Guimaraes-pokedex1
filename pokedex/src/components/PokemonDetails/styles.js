cc51d5d939149437bd6619f70913bdc855e948a4
Tiago Hennig <86529848+tiagohennig@users.noreply.github.com>
2022-04-16 19:41:37 -0300

    Update styles.js

---
+++ pokedex/src/components/PokemonDetails/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import react from 'react'
+
+
+
+export const headerDetails =styled.div`
+ display: flex;
+ background-color:pink;
+`
+ const buttonDetails =styled.div `
+ 
+ 
+ 
+ `
