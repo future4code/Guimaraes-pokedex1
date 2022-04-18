ffd3abb3f61f0f2a24350a01d17d79ef0d3f0e20
Tiago Hennig <tiago.hennig@hotmail.com>
2022-04-18 16:54:26 -0300

    .

--- pokedex/src/components/Pokedex/Pokedex.js
+++ pokedex/src/components/Pokedex/Pokedex.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PokemonsContext } from '../../global/GlobalStateContext';
 
 const Pokedex = () => {
+
+    const pokemons = useContext(PokemonsContext)
+
+    const eachPokemon = pokemons.map((poke) => {
+        return(
+            <p>{poke.name}</p>
+        )
+    }) 
+
     return(
         <div>
-            <p>POKEDEX</p>
+            <button>RENDERIZAR</button>
         </div>
     )
 }
